6a8e4a217d32e46895d0025962dfe7a37268bc48
nitro2850-oss <nitro2850@gmail.com>
2025-10-02 03:49:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
+from handlers.start_handler import start, setup_start_handlers
+import database
+import config
 
+def main():
+    database.init_db()
+    
+    app = ApplicationBuilder().token(config.BOT_TOKEN).build()
+    
+    app.add_handler(CommandHandler("start", start))
+    setup_start_handlers(app)
+    
+    print("🚀 البوت اشتغل!")
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
